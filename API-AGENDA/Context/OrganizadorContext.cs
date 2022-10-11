461f1d743630d7cf5df4b5c18fc0125f12ee31cf
Deivid Gonçalves <deividg2022@outlook.com>
2022-10-11 17:28:55 -0300

    first

---
+++ API-AGENDA/Context/OrganizadorContext.cs
@@ -0,0 +1,15 @@
+﻿using API_AGENDA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_AGENDA.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ API-AGENDA/Models/EnumTarefaStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace API_AGENDA.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado
+    }
+}
